ea4c830459bd36b96ee315d11f2483272d84fb4d
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-21 09:59:05 +0300

    the opening of the player by pressing the button of the same name has been released

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,23 @@ import { radioPlayerInit } from './radioPlayer.js';
 import { videoPlayerInit } from './videoPlayer.js';
 import { musicPlayerInit } from './musicPlayer.js';
 
+/* variables */
+const playerButtons = document.querySelectorAll('.player-btn');
+const playerBlocks = document.querySelectorAll('.player-block');
+
+const deactivationPlayer = () => {
+  playerButtons.forEach((item) => item.classList.remove('active'));
+  playerBlocks.forEach((item) => item.classList.remove('active'));
+};
+
+playerButtons.forEach((btn, index) =>
+  btn.addEventListener('click', () => {
+    deactivationPlayer();
+    btn.classList.add('active');
+    playerBlocks[index].classList.add('active');
+  })
+);
+
 radioPlayerInit();
 videoPlayerInit();
 musicPlayerInit();
