83b038378ae62d0424efb67e44a6090392623703
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-23 09:39:40 +0300

    got radio player elements

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,9 @@
 export const radioPlayerInit = () => {
-  console.log('radio init');
+  /* variables */
+  const radio = document.querySelector('.radio');
+  const radioCoverImg = document.querySelector('.radio-cover__img');
+  const radioNavigation = document.querySelector('.radio-navigation');
+  const radioHeader = document.querySelector('.radio-header');
+  const radioItem = document.querySelector('.radio-item');
+  const radioStop = document.querySelector('.radio-stop');
 };
