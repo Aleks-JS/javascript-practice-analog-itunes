467a81d55edb85f3bd0139e05c677cbd58c44266
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-23 10:26:27 +0300

    styled icon radiostation

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -14,16 +14,27 @@ export const radioPlayerInit = () => {
 
   const changeIconPlay = () => {
     if (audio.paused) {
+      radio.classList.remove('play');
       radioStop.classList.add('fa-play');
       radioStop.classList.remove('fa-stop');
     } else {
+      radio.classList.add('play');
       radioStop.classList.add('fa-stop');
       radioStop.classList.remove('fa-play');
     }
   };
 
+  const selectItem = (parent) => {
+    radioItem.forEach((item) => item.classList.remove('select'));
+    parent.classList.add('select');
+  };
+
   radioNavigation.addEventListener('change', (e) => {
     const target = e.target;
+    const parent = target.closest('.radio-item');
+
+    selectItem(parent);
+
     radioStop.disabled = false;
 
     audio.src = target.dataset.radioStation;
