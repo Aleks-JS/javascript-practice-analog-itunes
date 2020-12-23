8d2c89cad2250b1ad22513e464444a85137b7fac
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-23 09:54:53 +0300

    implemented turning on the radio when you press its button

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -4,6 +4,19 @@ export const radioPlayerInit = () => {
   const radioCoverImg = document.querySelector('.radio-cover__img');
   const radioNavigation = document.querySelector('.radio-navigation');
   const radioHeader = document.querySelector('.radio-header');
-  const radioItem = document.querySelector('.radio-item');
+  const radioItem = document.querySelectorAll('.radio-item');
   const radioStop = document.querySelector('.radio-stop');
+
+  const audio = new Audio();
+  audio.type = 'audio/aac';
+
+  radioStop.disabled = true;
+
+  radioNavigation.addEventListener('change', (e) => {
+    const target = e.target;
+
+    audio.src = target.dataset.radioStation;
+
+    audio.play();
+  });
 };
