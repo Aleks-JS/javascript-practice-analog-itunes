d7a1f1b6e1f323b487d524a6e547642f5f50f672
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-23 10:08:52 +0300

    implemented changed icon for button play/stop

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -12,11 +12,28 @@ export const radioPlayerInit = () => {
 
   radioStop.disabled = true;
 
+  const changeIconPlay = () => {
+    if (audio.paused) {
+      radioStop.classList.add('fa-play');
+      radioStop.classList.remove('fa-stop');
+    } else {
+      radioStop.classList.add('fa-stop');
+      radioStop.classList.remove('fa-play');
+    }
+  };
+
   radioNavigation.addEventListener('change', (e) => {
     const target = e.target;
+    radioStop.disabled = false;
 
     audio.src = target.dataset.radioStation;
 
     audio.play();
+    changeIconPlay();
+  });
+
+  radioStop.addEventListener('click', () => {
+    audio.paused ? audio.play() : audio.pause();
+    changeIconPlay();
   });
 };
